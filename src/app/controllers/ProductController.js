f9e2415a302563c0fef41da2947bd94be806d90a
jeffpereira <jeff.matheus18@gmail.com>
2020-09-16 20:34:00 -0300

    create post product method

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,48 @@
+const Category = require('../models/Category')
+const Product = require('../models/Product')
+
 module.exports = {
   create(req, res) {
-    return res.render('products/create.njk')
+    Category.all().then((results) => {
+      const categories = results.rows
+  
+    	return res.render('products/create.njk', { categories })
+
+    }).catch((err) => {
+      throw new Error(err)
+    })
+	},
+	
+  async post(req, res) {
+
+    const keys = Object.keys(req.body)
+
+		for (let key of keys) {
+			if (req.body[key] == '') {
+				return res.send('Please, fill all the fields!');
+			}
+    }
+	
+		req.body.price = req.body.price.replace(/\D/g, '')
+		
+		const values = [
+			req.body.category_id ,
+			req.body.user_id || 1,
+			req.body.name,
+			req.body.description,
+			req.body.old_price || req.body.price,
+			req.body.price,
+			req.body.quantity,
+			req.body.status || 1,
+    ]
+  
+  	let results = await Product.create(values)
+		const productId = results.rows[0].id
+
+		results = await Category.all()
+		const categories = results.rows
+
+		return res.render('products/create.njk', { productId, categories })
+		
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ routes.get('/', (req, res) => {
   return res.render('layout.njk')
 })
 routes.get('/products/create', ProductController.create)
+routes.post('/products', ProductController.post)
+
 routes.get('/ads/create', (req,res) => {
   return res.redirect('/products/create')
 })
