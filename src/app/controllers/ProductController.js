0415deb326e67b5b8be0be7c8bbd544060107130
jeffpereira <jeff.matheus18@gmail.com>
2020-09-16 20:40:58 -0300

    fix: redirect to edit page after post

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -39,10 +39,7 @@ module.exports = {
   	let results = await Product.create(values)
 		const productId = results.rows[0].id
 
-		results = await Category.all()
-		const categories = results.rows
-
-		return res.render('products/create.njk', { productId, categories })
+		return res.redirect(`/products/${productId}`)
 		
-  }
+	}
 }
