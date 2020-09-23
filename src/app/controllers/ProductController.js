a19a07b8461fe6b9cc2aff7e49be0050f81c0af9
jeffpereira <jeff.matheus18@gmail.com>
2020-09-23 09:09:35 -0300

    update imgs from a product

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -82,6 +82,13 @@ module.exports = {
 				return res.send('Please, fill all the fields!')
 		}
 
+		if (req.files.length != 0) {
+			const newFilesPromise = req.files.map(file => 
+				File.create({...file, product_id: req.body.id}))
+
+			await Promise.all(newFilesPromise)
+		}
+
 		if (req.body.removed_files) {
 			const removedFiles = req.body.removed_files.split(",")
 			const lastIndex = removedFiles.length - 1
