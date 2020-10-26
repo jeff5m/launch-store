46e206b962259d1b6217de3613a48080b76091cd
jeff5m <jeffmatheuspereira@gmail.com>
2020-10-26 20:41:41 -0300

    create users routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,12 @@ const routes = express.Router()
 const HomeController = require('../app/controllers/HomeController')
 
 const products = require('./products')
-// const users = require('./users')
+const users = require('./users')
 
 routes.get('/', HomeController.index)
 
 routes.use('/products', products)
-// routes.use('/users', users)
+routes.use('/users', users)
 
 routes.get('/ads/create', (req,res) => {
   return res.redirect('/products/create')
